dcc77275391cf3b0158b713039ab623f60c5ab83
Connor Rubattino <connor.rubattino@gmail.com>
2024-03-05 15:27:27 -0800

    finished

--- geometry.py
+++ geometry.py
@@ -1,6 +1,6 @@
 def circle_area(radius):
     return 3.14159265359 * (int(radius) ** 2)
 
-def hypotenuse(a, b):
-    c = (int(a) ** 2 + int(b) ** 2) ** .5
-        return c
+def hypotenuse(num1, num2):
+    hyp = ((num1 ** 2)+(num2 ** 2))** 0.5
+    return hyp
